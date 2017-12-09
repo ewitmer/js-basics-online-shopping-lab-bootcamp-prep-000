fe702729bee866e4a991762d291372805dd5206b
Erin Witmer <ecwitmer@gmail.com>
2017-12-09 02:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,6 +85,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (!cardNumber) {
-    console.log()
+    console.log("Sorry, we don't have a credit card on file for you.")
   }
 }
