702379e750bb82b6208b65c296f58eeac985f6e7
Erin Witmer <ecwitmer@gmail.com>
2017-12-09 02:59:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -88,6 +88,6 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     var total = total();
-    
+    console.log(`Your total cost is $71, which will be charged to the card 83296759.)
   }
 }
